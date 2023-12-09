f80b5761f058cc76733c215e5293e8cf33ec0643
Yusuf Ganiyu <airscholar@gmail.com>
2023-12-09 22:14:28 +0000

    added documentation to streamlit app

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,5 +1,4 @@
 import time
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,8 +8,9 @@ from kafka import KafkaConsumer
 from streamlit_autorefresh import st_autorefresh
 import psycopg2
 
-
+# Function to create a Kafka consumer
 def create_kafka_consumer(topic_name):
+    # Set up a Kafka consumer with specified topic and configurations
     consumer = KafkaConsumer(
         topic_name,
         bootstrap_servers='localhost:9092',
@@ -18,16 +18,20 @@ def create_kafka_consumer(topic_name):
         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
     return consumer
 
+# Function to fetch voting statistics from PostgreSQL database
 @st.cache_data
 def fetch_voting_stats():
+    # Connect to PostgreSQL database
     conn = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres")
     cur = conn.cursor()
 
+    # Fetch total number of voters
     cur.execute("""
         SELECT count(*) voters_count FROM voters
     """)
     voters_count = cur.fetchone()[0]
 
+    # Fetch total number of candidates
     cur.execute("""
         SELECT count(*) candidates_count FROM candidates
     """)
@@ -35,40 +39,22 @@ def fetch_voting_stats():
 
     return voters_count, candidates_count
 
-
-
+# Function to fetch data from Kafka
 def fetch_data_from_kafka(consumer):
+    # Poll Kafka consumer for messages within a timeout period
     messages = consumer.poll(timeout_ms=1000)
     data = []
 
+    # Extract data from received messages
     for message in messages.values():
         for sub_message in message:
             data.append(sub_message.value)
     return data
 
-
-st.title('Real-time Election Dashboard')
-topic_name = 'aggregated_votes_per_candidate'
-
-
-# sidebar
-def sidebar():
-    if st.session_state.get('last_update') is None:
-        st.session_state['last_update'] = time.time()
-
-    refresh_interval = st.sidebar.slider("Refresh interval (seconds)", 5, 60, 10)
-    st_autorefresh(interval=refresh_interval * 1000, key="auto")
-    if st.sidebar.button('Refresh Data'):
-        update_data()
-
-
+# Function to plot a colored bar chart for vote counts per candidate
 def plot_colored_bar_chart(results):
     data_type = results['candidate_name']
-
-    # Create a color map
     colors = plt.cm.viridis(np.linspace(0, 1, len(data_type)))
-
-    # plt.figure(figsize=(10, 4))
     plt.bar(data_type, results['total_votes'], color=colors)
     plt.xlabel('Candidate')
     plt.ylabel('Total Votes')
@@ -76,7 +62,7 @@ def plot_colored_bar_chart(results):
     plt.xticks(rotation=90)
     return plt
 
-
+# Function to plot a donut chart for vote distribution
 def plot_donut_chart(data: pd.DataFrame, title='Donut Chart', type='candidate'):
     if type == 'candidate':
         labels = list(data['candidate_name'])
@@ -84,37 +70,31 @@ def plot_donut_chart(data: pd.DataFrame, title='Donut Chart', type='candidate'):
         labels = list(data['gender'])
 
     sizes = list(data['total_votes'])
-    # Create a pie chart
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-
     ax.axis('equal')
     plt.title(title)
-
     return fig
 
-
+# Function to plot a pie chart for vote distribution
 def plot_pie_chart(data, title='Gender Distribution of Voters', labels=None):
-    sizes = list(data.values())  # Data values (e.g., vote counts)
+    sizes = list(data.values())
     if labels is None:
-        labels = list(data.keys())  # Data labels (e.g., candidate names)
+        labels = list(data.keys())
 
-    # Create a pie chart
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-
-    ax.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
+    ax.axis('equal')
     plt.title(title)
-
     return fig
 
-
+# Function to split a dataframe into chunks for pagination
 @st.cache_data(show_spinner=False)
 def split_frame(input_df, rows):
     df = [input_df.loc[i: i + rows - 1, :] for i in range(0, len(input_df), rows)]
     return df
 
-
+# Function to paginate a table
 def paginate_table(table_data):
     top_menu = st.columns(3)
     with top_menu[0]:
@@ -148,49 +128,34 @@ def paginate_table(table_data):
     pagination.dataframe(data=pages[current_page - 1], use_container_width=True)
 
 
+# Function to update data displayed on the dashboard
 def update_data():
-    last_refresh = st.empty()  # Placeholder to show last refresh time
+    # Placeholder to display last refresh time
+    last_refresh = st.empty()
     last_refresh.text(f"Last refreshed at: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
+    # Fetch voting statistics
     voters_count, candidates_count = fetch_voting_stats()
 
+    # Display total voters and candidates metrics
     st.markdown("""---""")
     col1, col2 = st.columns(2)
     col1.metric("Total Voters", voters_count)
     col2.metric("Total Candidates", candidates_count)
 
-    st.markdown("""---""")
-
+    # Fetch data from Kafka on aggregated votes per candidate
     consumer = create_kafka_consumer("aggregated_votes_per_candidate")
     data = fetch_data_from_kafka(consumer)
-
     results = pd.DataFrame(data)
-    # get max results for each candidate
+
+    # Identify the leading candidate
     results = results.loc[results.groupby('candidate_id')['total_votes'].idxmax()]
-    # get the photo url for each candidate
-    candidates_name_and_photo = results[['candidate_name', 'photo_url', 'party_affiliation']].to_dict('records')
+    leading_candidate = results.loc[results['total_votes'].idxmax()]
 
-    st.header('Candidates')
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.image(candidates_name_and_photo[0]['photo_url'], width=200)
-        st.write(candidates_name_and_photo[0]['candidate_name'] + " (" + candidates_name_and_photo[0][
-            'party_affiliation'] + ")")
-    with col2:
-        st.image(candidates_name_and_photo[1]['photo_url'], width=200)
-        st.write(candidates_name_and_photo[1]['candidate_name'] + " (" + candidates_name_and_photo[1][
-            'party_affiliation'] + ")")
-    with col3:
-        st.image(candidates_name_and_photo[2]['photo_url'], width=200)
-        st.write(candidates_name_and_photo[2]['candidate_name'] + " (" + candidates_name_and_photo[2][
-            'party_affiliation'] + ")")
-
-    # horizontal line
+    # Display leading candidate information
     st.markdown("""---""")
-
     st.header('Leading Candidate')
     col1, col2 = st.columns(2)
-    leading_candidate = results.loc[results['total_votes'].idxmax()]
     with col1:
         st.image(leading_candidate['photo_url'], width=200)
     with col2:
@@ -198,16 +163,14 @@ def update_data():
         st.subheader(leading_candidate['party_affiliation'])
         st.subheader("Total Vote: {}".format(leading_candidate['total_votes']))
 
-    # horizontal line
+    # Display statistics and visualizations
     st.markdown("""---""")
-
     st.header('Statistics')
     results = results[['candidate_id', 'candidate_name', 'party_affiliation', 'total_votes']]
-    # remove the index column
     results = results.reset_index(drop=True)
     col1, col2 = st.columns(2)
 
-    # Assuming 'data' is a dictionary with candidate names and vote counts
+    # Display bar chart and donut chart
     with col1:
         bar_fig = plot_colored_bar_chart(results)
         st.pyplot(bar_fig)
@@ -216,20 +179,45 @@ def update_data():
         donut_fig = plot_donut_chart(results, title='Vote Distribution')
         st.pyplot(donut_fig)
 
+    # Display table with candidate statistics
     st.table(results)
 
+    # Fetch data from Kafka on aggregated turnout by location
     location_consumer = create_kafka_consumer("aggregated_turnout_by_location")
     location_data = fetch_data_from_kafka(location_consumer)
     location_result = pd.DataFrame(location_data)
 
+    # Identify locations with maximum turnout
     location_result = location_result.loc[location_result.groupby('state')['count'].idxmax()]
     location_result = location_result.reset_index(drop=True)
 
+    # Display location-based voter information with pagination
     st.header("Location of Voters")
     paginate_table(location_result)
 
+    # Update the last refresh time
     st.session_state['last_update'] = time.time()
 
+# Sidebar layout
+def sidebar():
+    # Initialize last update time if not present in session state
+    if st.session_state.get('last_update') is None:
+        st.session_state['last_update'] = time.time()
 
+    # Slider to control refresh interval
+    refresh_interval = st.sidebar.slider("Refresh interval (seconds)", 5, 60, 10)
+    st_autorefresh(interval=refresh_interval * 1000, key="auto")
+
+    # Button to manually refresh data
+    if st.sidebar.button('Refresh Data'):
+        update_data()
+
+# Title of the Streamlit dashboard
+st.title('Real-time Election Dashboard')
+topic_name = 'aggregated_votes_per_candidate'
+
+# Display sidebar
 sidebar()
+
+# Update and display data on the dashboard
 update_data()
