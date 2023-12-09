ef89935386a0fcd4e38d199422512062f3af8d31
Yusuf Ganiyu <airscholar@gmail.com>
2023-12-09 22:01:57 +0000

    updated candidates and voters generation logic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import simplejson as json
+import random
 
+import psycopg2
 import requests
+import simplejson as json
 from confluent_kafka import SerializingProducer
 
 BASE_URL = 'https://randomuser.me/api/?nat=gb'
+PARTIES = ["Management Party", "Savior Party", "Tech Republic Party"]
+random.seed(42)
 
 
 def generate_voter_data():
@@ -12,7 +16,7 @@ def generate_voter_data():
         user_data = response.json()['results'][0]
         return {
             "voter_id": user_data['login']['uuid'],
-            "name": f"{user_data['name']['first']} {user_data['name']['last']}",
+            "voter_name": f"{user_data['name']['first']} {user_data['name']['last']}",
             "date_of_birth": user_data['dob']['date'],
             "gender": user_data['gender'],
             "nationality": user_data['nat'],
@@ -35,14 +39,15 @@ def generate_voter_data():
 
 
 def generate_candidate_data(candidate_number, total_parties):
-    response = requests.get(BASE_URL + '&seed=voting_app')
+    response = requests.get(BASE_URL + '&gender=' + ('female' if candidate_number % 2 == 1 else 'male'))
     if response.status_code == 200:
         user_data = response.json()['results'][0]
-        party_affiliation = f"Party {chr(65 + candidate_number % total_parties)}"  # Party A, B, C, etc.
+
+
         return {
             "candidate_id": user_data['login']['uuid'],
-            "name": f"{user_data['name']['first']} {user_data['name']['last']}",
-            "party_affiliation": party_affiliation,
+            "candidate_name": f"{user_data['name']['first']} {user_data['name']['last']}",
+            "party_affiliation": PARTIES[candidate_number % total_parties],
             "biography": "A brief bio of the candidate.",
             "campaign_platform": "Key campaign promises or platform.",
             "photo_url": user_data['picture']['large']
@@ -51,43 +56,113 @@ def generate_candidate_data(candidate_number, total_parties):
         return "Error fetching data"
 
 
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+
 # Kafka Topics
 voters_topic = 'voters_topic'
 candidates_topic = 'candidates_topic'
 
-if __name__ == "__main__":
-    producer = SerializingProducer(
-        {
-            'bootstrap.servers': 'localhost:9092',
-        }
-    )
 
-    # Simulate Candidate Data Generation and Sending to Kafka
-    candidates = []
-    for i in range(2):
-        candidate_data = generate_candidate_data(i, 3)
-        candidates.append(candidate_data)
-        producer.produce(
-            candidates_topic,
-            key=candidate_data["candidate_id"],
-            value=json.dumps(candidate_data)
+def create_tables(conn, cur):
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS candidates (
+            candidate_id VARCHAR(255) PRIMARY KEY,
+            candidate_name VARCHAR(255),
+            party_affiliation VARCHAR(255),
+            biography TEXT,
+            campaign_platform TEXT,
+            photo_url TEXT
         )
+    """)
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS voters (
+            voter_id VARCHAR(255) PRIMARY KEY,
+            voter_name VARCHAR(255),
+            date_of_birth VARCHAR(255),
+            gender VARCHAR(255),
+            nationality VARCHAR(255),
+            registration_number VARCHAR(255),
+            address_street VARCHAR(255),
+            address_city VARCHAR(255),
+            address_state VARCHAR(255),
+            address_country VARCHAR(255),
+            address_postcode VARCHAR(255),
+            email VARCHAR(255),
+            phone_number VARCHAR(255),
+            cell_number VARCHAR(255),
+            picture TEXT,
+            registered_age INTEGER
+        )
+    """)
 
-    # Simulate Voter Data Generation and Sending to Kafka
-    # This can be a loop or triggered based on certain conditions
-    for i in range(10000):
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS votes (
+            voter_id VARCHAR(255) UNIQUE,
+            candidate_id VARCHAR(255),
+            voting_time TIMESTAMP,
+            vote int DEFAULT 1,
+            PRIMARY KEY (voter_id, candidate_id)
+        )
+    """)
+
+    conn.commit()
+
+
+def insert_voters(conn, cur, voter):
+    cur.execute("""
+                        INSERT INTO voters (voter_id, voter_name, date_of_birth, gender, nationality, registration_number, address_street, address_city, address_state, address_country, address_postcode, email, phone_number, cell_number, picture, registered_age)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s)
+                        """,
+                (voter["voter_id"], voter['voter_name'], voter['date_of_birth'], voter['gender'],
+                 voter['nationality'], voter['registration_number'], voter['address']['street'],
+                 voter['address']['city'], voter['address']['state'], voter['address']['country'],
+                 voter['address']['postcode'], voter['email'], voter['phone_number'],
+                 voter['cell_number'], voter['picture'], voter['registered_age'])
+                )
+    conn.commit()
+
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres")
+    cur = conn.cursor()
+
+    producer = SerializingProducer({'bootstrap.servers': 'localhost:9092', })
+    create_tables(conn, cur)
+
+    # get candidates from db
+    cur.execute("""
+        SELECT * FROM candidates
+    """)
+    candidates = cur.fetchall()
+    print(candidates)
+
+    if len(candidates) == 0:
+        for i in range(3):
+            candidate = generate_candidate_data(i, 3)
+            print(candidate)
+            cur.execute("""
+                        INSERT INTO candidates (candidate_id, candidate_name, party_affiliation, biography, campaign_platform, photo_url)
+                        VALUES (%s, %s, %s, %s, %s, %s)
+                    """, (
+                candidate['candidate_id'], candidate['candidate_name'], candidate['party_affiliation'], candidate['biography'],
+                candidate['campaign_platform'], candidate['photo_url']))
+            conn.commit()
+
+    for i in range(1000):
         voter_data = generate_voter_data()
+        insert_voters(conn, cur, voter_data)
+
         producer.produce(
             voters_topic,
             key=voter_data["voter_id"],
-            value=json.dumps(voter_data)
+            value=json.dumps(voter_data),
+            on_delivery=delivery_report
         )
 
+        print('Produced voter {}, data: {}'.format(i, voter_data))
         producer.flush()
-
-    # print(json.dumps(candidates, indent=4))
-
-    # while True:
-    # voter_data = generate_voter_data()
-    # print(json.dumps(voter_data, indent=4))
-    # time.sleep(1)  # Simulate real-time data generation
